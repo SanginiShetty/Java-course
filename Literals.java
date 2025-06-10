6cd108bab810e2cc82947b3cf5ea8fba012d4367
“Sangini <sanginijshetty@gmail.com>
2025-06-10 22:55:33 +0530

    .

--- Literals.java
+++ Literals.java
@@ -4,6 +4,18 @@ public class Literals {
         //binary
         int num = 0b110;
         System.out.println(num);
+
+        //hexadecimal
+        int num1 = 0x7E;
+        System.out.println(num1);
+
+        //Counting number of zeros
+        int num2 = 1_00_00_00_000;
+        System.out.println(num2);
+
+        //double
+        double num3 = 59;
+        System.out.println(num3);
     }
     
 }
