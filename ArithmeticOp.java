b24c560b3b79ade8cf6d368c420721240e7bb90c
“Sangini <sanginijshetty@gmail.com>
2025-06-16 21:20:39 +0530

    .

---
+++ ArithmeticOp.java
@@ -0,0 +1,21 @@
+public class ArithmeticOp {
+    public static void main(String[] args) {
+        int a = 7;
+        int b =5;
+        int add = a+b;
+        System.out.println(add);
+
+        int sub = a-b;
+        System.out.println(sub);
+
+        int prod = a*b;
+        System.out.println(prod);
+
+        int div = a/b;
+        System.out.println(div);
+
+        int mod = a % b;
+        System.out.println(mod);
+
+    }
+}
