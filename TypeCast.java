ad19afa5ad0d81160a1ecee465f79e29b08ef912
“Sangini <sanginijshetty@gmail.com>
2025-06-16 21:03:30 +0530

    .

--- TypeCast.java
+++ TypeCast.java
@@ -1,7 +1,21 @@
 public class TypeCast {
     public static void main(String args[]) {
-        byte b = 127;
-        int a = b;
-        System.out.println(a);
+        // byte b = 127;
+        // int a = b;
+        // System.out.println(a);
+
+        // int a = 259;
+        // byte k = (byte)a;
+        // System.out.println(k);
+
+        // float f = 5.6f;
+        // int t = (int) f;
+        // System.out.println(t);
+
+        //Type promotion
+        byte a = 10;
+        byte b = 30;
+        int result = a*b;
+        System.out.println(result);
     }
 }
